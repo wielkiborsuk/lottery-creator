085ab29cd26496b879980ddb6dc0e55a0f394296
wielki.borsuk <wielki.borsuk@gmail.com>
2020-03-12 14:39:54 +0100

    add csv loading, change draw to avoid duplicates

--- lotterycreator.py
+++ lotterycreator.py
@@ -1,5 +1,14 @@
 import json
+import csv
 import random
+
+
+def load_csv_file(name):
+    with open(name, 'r') as f:
+        lines = list(csv.reader(f))
+        keys = lines[0]
+        return [{keys[i]: line[i] for i in range(len(line))}
+                for line in lines[1:]]
 
 
 def load_json_file(name):
@@ -8,15 +17,34 @@ def load_json_file(name):
         return participants
 
 
-def main():
-    input_file = ''
-    # file_format = ''
-    winner_count = 1
+def load_participants_info(format, name):
+    if format == 'csv':
+        return load_csv_file(name)
+    elif format == 'json':
+        return load_json_file(name)
+    else:
+        return []
 
-    participants = load_json_file(input_file)
-    weights = [float(p.get('weight', '1')) for p in participants]
-    winners = random.choices(participants, weights=weights, k=winner_count)
-    print(winners)
+
+def draw(participants, winner_count):
+    winners = []
+    for i in range(winner_count):
+        weights = [float(p.get('weight', '1')) for p in participants]
+        (winner, ) = random.choices(participants, weights=weights)
+        winners.append(winner)
+        participants.remove(winner)
+    return winners
+
+
+def main():
+    input_file = '../data/participants2.json'
+    file_format = 'json'
+    winner_count = 3
+
+    participants = load_participants_info(file_format, input_file)
+    winners = draw(participants, winner_count)
+    for winner in winners:
+        print('{} {}'.format(winner['first_name'], winner['last_name']))
 
 
 if __name__ == "__main__":
